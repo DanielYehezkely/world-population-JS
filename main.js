5face71b9e62522bfb840d597aba5eacb139745a
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-05-23 20:31:27 +0300

    finished building chart.js

---
+++ main.js
@@ -0,0 +1,8 @@
+import { ChartInitializer } from "./modules/chart.js";
+import { fetchCountries } from "./modules/fetchCountries.js";
+
+const init = new ChartInitializer();
+
+
+
+fetchCountries()
---
+++ modules/chart.js
@@ -0,0 +1,28 @@
+export class ChartInitializer {
+  constructor() {
+      this.initialize();
+  }
+
+  initialize() {
+    const ctx = document.getElementById('chart');
+
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+}
