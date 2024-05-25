13f24b17e23180db8c329bc112e3eae0d300e0ba
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-05-25 14:32:25 +0300

    fixing bugs

--- modules/loader.js
+++ modules/loader.js
@@ -1,17 +1,7 @@
 export default class Loader {
   constructor(continentsContainer) {
-    this.loaderContainer = continentsContainer;
-    this.originalClassName = continentsContainer.className;
-    this.originalHtml = continentsContainer.innerHTML;
+    
   }
 
-  showLoader() {
-    this.loaderContainer.innerHTML = '';
-    this.loaderContainer.className = 'loader';
-  }
-
-  hideLoader() {
-    this.loaderContainer.className = this.originalClassName;
-    this.loaderContainer.innerHTML = this.originalHtml;
-  }
+ 
 }
