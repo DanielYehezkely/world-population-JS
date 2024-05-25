85572eca8d106b1dedd311700b0f06a80fb64882
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-05-25 22:34:33 +0300

    Finished fixing another bugs

--- modules/app.js
+++ modules/app.js
@@ -2,7 +2,7 @@ import { COUNTRIES_URL, CITIES_URL } from "../models/constants.js";
 import ApiHandler from "../api/api.js";
 import ChartHandler from "../modules/chart.js";
 import ErrorHandler from "../modules/error.js";
-import Loader from "../modules/loader.js"; 
+import Loader from "../modules/loader.js";
 
 export default class App {
   constructor() {
@@ -10,9 +10,11 @@ export default class App {
     this.continentsContainer = document.getElementById('continents-container');
     this.chartContainer = document.getElementById('chart-container')
     this.countriesContainer = document.getElementById('countries-container');
+    this.countryMenu = document.getElementById('country-menu')
+    this.menuButton = document.getElementById('menu-button');
     this.apiHandler = new ApiHandler(COUNTRIES_URL, CITIES_URL);
     this.errorHandler = new ErrorHandler();
-    this.loader = new Loader(this.continentsContainer, this.chartContainer, this.countriesContainer ); 
+    this.loader = new Loader(this.continentsContainer, this.chartContainer, this.countriesContainer);
     this.chartHandler = new ChartHandler();
     this.handleResize();
   }
@@ -22,7 +24,7 @@ export default class App {
       button.addEventListener('click', async (event) => {
         try {
           const region = event.target.dataset.name;
-          this.loader.showLoader(); 
+          this.loader.showLoader();
           const countries = await this.apiHandler.fetchCountries(region);
           this.renderCountries(countries);
           this.chartHandler.showChart();
@@ -41,7 +43,7 @@ export default class App {
       button.addEventListener('click', async (event) => {
         try {
           const countryName = event.target.textContent.toLowerCase();
-          this.loader.showLoader(); 
+          this.loader.showLoader();
           const response = await this.apiHandler.fetchCities(countryName);
           if (response && response.data) {
             this.countriesContainer.innerHTML = countryName.toUpperCase();
@@ -51,7 +53,7 @@ export default class App {
         } catch (error) {
           this.errorHandler.displayError(error.message);
         } finally {
-          this.loader.hideLoader(); 
+          this.loader.hideLoader();
         }
       });
     });
@@ -94,13 +96,36 @@ export default class App {
     this.handleCountryClick();
   }
 
-  handleResize() {
-    window.addEventListener('resize', () => {
+  async handleResize() {
+    window.addEventListener('resize', async () => {
       if (window.innerWidth < 600) {
         this.countriesContainer.style.display = 'none';
-      } 
+        this.continentButtons.forEach(button => {
+          button.addEventListener('click', async (event) => {
+            try {
+              const region = event.target.dataset.name;
+              this.loader.showLoader();
+              const countries = await this.apiHandler.fetchCountries(region);
+              this.countryMenu.innerHTML = countries.map(country => `
+      <button class="btn">${country.name.common}</button>
+    `).join('');
+              this.menuButton.classList.remove('hidden')
+              this.menuButton.addEventListener('click', () => {
+                if (this.countryMenu.classList.contains('hidden')) {
+                  this.countryMenu.classList.remove('hidden');
+                } else {
+                  this.countryMenu.classList.add('hidden');  // refactor that immediately after the bugs
+                }
+              })
+            } catch {
+              console.error('error');
+            };
+          })
+        })
+      }
       if (window.innerWidth > 600) {
-        this.countriesContainer.style.display = 'flex'; // got o fix bugs here
+        this.countriesContainer.style.display = 'flex'; // got o fix bugs here show the container with loader
+
       }
     });
     window.dispatchEvent(new Event('resize'));
