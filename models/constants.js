673f109796e8d9e09223e0c03076391d27dcb803
DanielYehezkely <135972497+DanielYehezkely@users.noreply.github.com>
2024-05-25 18:02:06 +0300

    fixing bugs

--- models/constants.js
+++ models/constants.js
@@ -2,6 +2,6 @@
 export const COUNTRIES_URL = 'https://restcountries.com/v3.1/all';
 export const CITIES_URL = 'https://countriesnow.space/api/v0.1/countries/population/cities/filter';
 
-// ========================= DOM ELEMENTS =================================================
+
 
 
